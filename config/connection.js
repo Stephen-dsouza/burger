5fc14426e522330f48378f137ef1165bfaa3a600
Stephen <stephen.roy.dsouza@gmail.com>
2019-09-24 18:20:18 +1000

    ADDING JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 var mysql = require("mysql");
 require("dotenv").config();
 var keys = require("./keys");
-var connection = mysql.createConnection((keys.sql));
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
 
+var connection;
+if(process.env.JAWSDB_URL){
+  connection=mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection((keys.sql));
+}
+// var connection = mysql.createConnection((keys.sql));
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+connection.connect();
 module.exports = connection;
